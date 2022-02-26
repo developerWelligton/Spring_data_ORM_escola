97dc6e2e55d8fb04666278845453efc2af4d806c
developerWelligton <developerwelligton@gmail.com>
2022-02-26 16:14:38 -0300

    configuração inicial

---
+++ spring-data-escola/spring-data-escola/src/main/java/br/com/welligton/spring/data/escola/SpringDataEscolaApplication.java
@@ -0,0 +1,13 @@
+package br.com.welligton.spring.data.escola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataEscolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataEscolaApplication.class, args);
+	}
+
+}
---
+++ spring-data-escola/spring-data-escola/src/test/java/br/com/welligton/spring/data/escola/SpringDataEscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.welligton.spring.data.escola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataEscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
